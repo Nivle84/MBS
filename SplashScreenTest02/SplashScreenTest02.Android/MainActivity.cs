5cc6e3a7aac608be72f26ba196f261610782538e
Elvin Emil Andersen Jensen <elvi0984@elev.tec.dk>
2021-12-08 09:43:33 +0100

    Before experimenting with EF Core, SQL

--- SplashScreenTest02/SplashScreenTest02.Android/MainActivity.cs
+++ SplashScreenTest02/SplashScreenTest02.Android/MainActivity.cs
@@ -17,6 +17,7 @@ namespace SplashScreenTest02.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            //LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
